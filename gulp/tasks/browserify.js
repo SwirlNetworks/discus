921258f7b2ee3d01152b9b52e20ac076307d1e34
Chris Vickery <chrisinajar@gmail.com>
2015-02-20 18:40:54 -0500

    Added minified output as well

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var config = require('../config');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 
 var plumber = require('gulp-plumber');
 
@@ -14,5 +14,9 @@ gulp.task('browserify', function() {
 			standalone: 'Discus'
 		}))
 		.pipe(rename('discus.js'))
-		.pipe(gulp.dest(config.dest));
+		.pipe(gulp.dest(config.dest))
+		.pipe(uglify({
+		}))
+		.pipe(rename('discus.min.js'))
+		.pipe(gulp.dest(config.dest))
 });
