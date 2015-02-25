926dee9f7fd9728115ff85e10404cf4fb823b170
Chris Vickery <chrisinajar@gmail.com>
2015-02-24 21:00:41 -0500

    filled in table entry a bit

--- src/table_entry.js
+++ src/table_entry.js
@@ -1,7 +1,16 @@
+_ = require('underscore');
 Discus = require('./discus');
 
 Discus.TableEntry = Discus.View.extend({
+	template: _.template([
+		'<td>It worked!</td>'
+	].join('')),
 
+	tagName: 'tr',
+
+	initialize: function() {
+		console.log('This is a table entry!');
+	}
 });
 
 module.exports = Discus.TableView;
